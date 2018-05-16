0301787c294a76f2d3ec534327f32582b97f551b
Antonio Carlos <baptistaacbc@hotmail.com>
2018-05-16 18:42:03 -0300

    MVP1 consolidação dos arquivos python em um único .py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from Preprocessor import Preprocessor
-from matrix	import matrix as mtx
+#from matrix	import matrix as mtx
 if __name__ == "__main__":
 	preprocessor = Preprocessor()
 	texts = preprocessor.readAllTextsFromDatabase()
 	#texts contêm todos os textos que serão utilizados de forma que cada index do array tem uma notícia. As notícias não estão tratadas , são o texto puro , retirado apenas os e-mails e em ordem aleatória.
-	texts = preprocessor.processTexts(texts)	
+	texts = preprocessor.processTexts(texts)
