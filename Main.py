ba1ff0f7d166d9db4178fb5cef704304cf9c539c
Antonio Carlos <baptistaacbc@hotmail.com>
2018-05-27 13:44:53 -0300

    Ajustando classe Main para rodar preprocessamento, matriz e dps kmeans

--- Main.py
+++ Main.py
@@ -35,37 +35,13 @@ if __name__ == "__main__":
 	"""
 		[X] - Ler todos os textos
 		[X] - Fazer data clean dos dados
-		[] - Roda Bag of Words para transformar lista de textos em vetor bidimensional de frequencia de palavra por texto
-		[] - Criar 3 outputs do Bag of Words
-			[] - Matrix binaria
-			[] - Matrix tf
-			[] - Matrix tf_idf
+		[X] - Roda Bag of Words para transformar lista de textos em vetor bidimensional de frequencia de palavra por texto
+		[X] - Criar 3 outputs do Bag of Words
+			[X] - Matrix binaria
+			[X] - Matrix tf
+			[X] - Matrix tf_idf
+			[] - Ngrams
 		[] - Rodar K-means para cada matrix
 		[] - Rodar SOM para cada matrix
 		[] - Pos-processamento
-	"""
-
-	""" Teste KMeans
-	n = 10000
-    dimentions = 10
-    NGroups = 5
-    iterations = 200
-
-    data = ListOfPoints(n, dimentions)
-    data.points = [[random()*1000 for j in range(dimentions)] for i in range(n)]
-    kmeans = KMeans(data, NGroups)
-
-    print('Inicializando execução')
-    print(str(n) + ' elementos de dados')
-    print(str(dimentions) + ' dimenções')
-    print(str(NGroups) + ' grupos')
-    print(str(iterations) + ' iterações')
-    print('')
-    print('Iteração\tNúmero de elementos em cada grupo')
-
-    for i in range(iterations):
-        print(str(i+1) + '/' + str(iterations) + '\t\t', end='')
-        kmeans.run()
-
-    input()
 	"""
